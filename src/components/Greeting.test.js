6daf19e91c7d6ee86f00adc320eaf3954bffdc63
Ajay-1502 <ahdesai40@gmail.com>
2025-08-14 10:02:56 +0530

    Written basic test using jest & rtl

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import Greeting from './Greeting';
+
+test('Testing greeting component', () => {
+  render(<Greeting />);
+  const helloWorldElement = screen.getByText('Hello World');
+  const paraElement = screen.getByText("It's good to see you", {
+    exact: false,
+  });
+  expect(paraElement).toBeInTheDocument();
+  expect(helloWorldElement).toBeInTheDocument();
+});
