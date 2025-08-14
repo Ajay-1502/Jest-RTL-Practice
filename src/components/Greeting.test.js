ecb91e6cf97cd27c49ecfdc3be6af7de3351c95b
Ajay-1502 <ahdesai40@gmail.com>
2025-08-14 10:11:01 +0530

    Grouped tests together using test suite concept

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,12 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('Testing greeting component', () => {
-  render(<Greeting />);
-  const helloWorldElement = screen.getByText('Hello World');
-  const paraElement = screen.getByText("It's good to see you", {
-    exact: false,
+describe('Greeting component', () => {
+  test('Testing greeting component', () => {
+    render(<Greeting />);
+    const helloWorldElement = screen.getByText('Hello World');
+    const paraElement = screen.getByText("It's good to see you", {
+      exact: false,
+    });
+    expect(paraElement).toBeInTheDocument();
+    expect(helloWorldElement).toBeInTheDocument();
   });
-  expect(paraElement).toBeInTheDocument();
-  expect(helloWorldElement).toBeInTheDocument();
 });
